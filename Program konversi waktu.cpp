16e7f8f3d7cd631f055b8dff42109fc0f2db23e8
AminPriadi <45525407+AminPriadi@users.noreply.github.com>
2019-01-05 10:47:11 +0700

    Add files via upload

---
+++ Program konversi waktu.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int hari, jam, menit, detik, sisa1, sisa2, sisa3;
+    cout<<"masukan jumlah detik ";
+    cin>>detik;
+
+    hari= detik/86400;
+    sisa1= detik- (hari*86400);
+    jam= sisa1/3600;
+    sisa2= sisa1- (jam*3600);
+    menit= sisa2/60;
+    sisa3= sisa2- (menit*60);
+    detik= sisa3;
+
+    cout<<hari<<" hari "<<jam<<" jam "<<menit<<" menit "<<detik<<" detik";
+
+    return 0;
+}
